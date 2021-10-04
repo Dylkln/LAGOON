a20973284fa8f2d83b5b121daff34dd4af76f1d3
Dylkln <klein.dylan@outlook.com>
2021-10-04 17:36:33 +0200

    update test.py

--- test/test.py
+++ test/test.py
@@ -1,61 +1,43 @@
 """This script is used to create all test files used to test all scripts"""
 
-
-#================================== Modules ==================================#
+# ================================== Modules ==================================#
 
 import argparse
 
-#=============================================================================#
+
+# =============================================================================#
 
 def arguments():
     """
     set arguments
     """
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="This script is used to test \
+your program installation and to familiarize yourself with the available \
+arguments. Please see the list of arguments below.")
 
-    # Mandatory arguments
-    parser.add_argument("-f", "--file", dest = "file",
-        type = str, required = True,
-        help = "the name of the input file")
+    # Optional Arguments
+    parser.add_argument("-pt", "--pattern", dest="pattern",
+                        type=str, required=False,
+                        help="the pattern of the files to be found")
+    parser.add_argument("-ph", "--path", dest="path",
+                        type=str, required=False,
+                        help="absolute path to directory for the pattern")
 
     return parser.parse_args()
 
-def read_file(file):
 
+def read_file(file):
     with open(file) as f:
         for line in f:
             yield line.strip()
 
-def get_data(file):
-
-    for index, line in enumerate(read_file(file)):
-        llist = line.split()
-        if index == 0:
-            if llist[0] == llist[1]:
-                eq = set(llist)
-            elif llist[0] != llist[1]:
-                neq = set(llist)
-            else:
-                eq = set()
-                neq = set()
-                oth = set(llist)
-        else:
-            if llist[0] == llist[1]:
-                if len(eq) <= 10:
-                    eq.add(llist)
-            elif llist[0] != llist[1]:
-                if len(neq) <= 10:
-                    neq.add(llist)
-            else:
-                if len(oth) <= 50:
-                    oth.add(llist)
-        if len(eq) == 10 and len(neq) == 10 and len(oth) == 50:
-            return eq, neq, oth
-
-    return eq, neq, oth
 
 def main():
-
     args = arguments()
-    eq, neq, oth = get_data(args.file)
+    if not args:
+        print("Please enter some arguments, see --help for more infos")
+
+
+if __name__ == '__main__':
+    main()
