1f73f686f7c69c2316c7d80b62a380803695dd52
Dylkln <klein.dylan@outlook.com>
2021-10-06 11:09:09 +0200

    update readme and test.py

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-Initial commit
+## PROGRAM USED FOR THE STUDY OF TROPHIC MODES IN PROTISTS
+
+## Prerequisites
+
+The use of [Miniconda3](https://docs.conda.io/en/latest/miniconda.html) is strongly recommended.
+
+It is important that your data is standardized.
+The format of your data must be like this :
+
+
+## Download the program
+
+1. Clone git repository
+
+> HTTPS link
+
+```
+git clone https://github.com/Dylkln/EGmnhn.git
+```
+
+> SSH link
+
+```
+git clone git@github.com:Dylkln/EGmnhn.git
+```
+
+2. Initialize conda environment using the file *environment.yml*
+
+```
+conda env create --file environment.yml
+```
+
+3. Activate conda environment
+
+```
+conda activate trophic
+```
--- test/test.py
+++ test/test.py
@@ -1,28 +1,34 @@
-"""This script is used to create all test files used to test all scripts"""
+"""This script is used to test your program installation and to familiarize yourself with the available
+arguments. Please see the list of arguments below."""
 
-# ================================== Modules ==================================#
+# ================================== Modules ================================== #
 
 import argparse
+import os
+import sys
+from Bio import SeqIO
+from modules import find as fi
+from modules import cat
 
-
-# =============================================================================#
+# ============================================================================= #
 
 def arguments():
     """
     set arguments
     """
 
-    parser = argparse.ArgumentParser(description="This script is used to test \
-your program installation and to familiarize yourself with the available \
-arguments. Please see the list of arguments below.")
+    parser = argparse.ArgumentParser(description="This script is used to test your program installation and to \
+familiarize yourself with the available arguments. Please see the list of arguments below.")
 
     # Optional Arguments
-    parser.add_argument("-pt", "--pattern", dest="pattern",
+    parser.add_argument("-tr", dest="fasta_files",
                         type=str, required=False,
-                        help="the pattern of the files to be found")
-    parser.add_argument("-ph", "--path", dest="path",
+                        help="either a path to find files in fasta format or a file containing the list of all path \
+to files in fasta format.")
+    parser.add_argument("-an", dest="annotation_files",
                         type=str, required=False,
-                        help="absolute path to directory for the pattern")
+                        help="either a path to find annotation files or a file containing the list of all path \
+to annotation files.")
     parser.add_argument("-ov", "--overlap", dest="overlap",
                         type=float, required=False, default=None, nargs="+",
                         help="the filtration wanted of a diamond output")
@@ -33,8 +39,17 @@ arguments. Please see the list of arguments below.")
     return parser.parse_args()
 
 
+def read_fasta_files(files, output):
+    for file in files
+        f_seq = SeqIO.parse(open(file), "fasta")
+        with open(output, "w") as out:
+            for s in f_seq:
+                out.write(s)
+
+
+
 def read_file(file):
-    with open(file) as f:
+    with open(file, "r") as f:
         for line in f:
             yield line.strip()
 
@@ -43,6 +58,16 @@ def main():
     args = arguments()
     if not args:
         print("Please enter some arguments, see --help / -h for more infos")
+
+    if args.path and args.pattern:
+        files = fi.find_file(args.pattern, args.path)
+        path = "./results/"
+        if not os.path.exists(path):
+            os.mkdir(path)
+    elif args.overlap and args.identity:
+        pass
+    else:
+        pass
 
 
 if __name__ == '__main__':
